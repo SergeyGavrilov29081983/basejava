009210552696b916169ca54b912792f79a67af39
sergo7777777 <sierghiei_gavrilov_1983@mail.ru>
2018-05-13 19:39:35 +0100

    version1.04

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -7,18 +7,20 @@ import java.util.Arrays;
 public class ArrayStorage {
 
     Resume[] storage = new Resume[10000];
-    int size = storage.length;
+    private int size;
 
 
     void clear() {
         storage = new Resume[10000];
+        size = 0;
     }
 
     void save(Resume r) {
-        for (int i = 0; i < size; i++) {
+        for (int i = 0; i < storage.length; i++) {
             if (storage[i] != null) {
             } else {
                 storage[i] = r;
+                size += 1;
                 break;
             }
         }
@@ -35,29 +37,23 @@ public class ArrayStorage {
 
     void delete(String uuid) {
         int i = 0;
-        for (i = 0; i < size(); i++) {
+        for (i = 0; i < size; i++) {
             if (storage[i].uuid == uuid) {
                 break;
             }
         }
-        System.arraycopy(storage, i + 1, storage, i, size - 1 - i);
+        System.arraycopy(storage, i + 1, storage, i, storage.length - 1 - i);
+        size -= 1;
     }
 
     Resume[] getAll() {
-        Resume[] resume = new Resume[size()];
-        System.arraycopy(storage, 0, resume, 0, size());
+        Resume[] resume = new Resume[size];
+        System.arraycopy(storage, 0, resume, 0, size);
         return resume;
     }
 
     int size() {
-        int count = 0;
-        for (Resume aStorage : storage) {
-            if (aStorage != null) {
-                count += 1;
-            } else {
-                break;
-            }
-        }
-        return count;
+        return size;
     }
+
 }
