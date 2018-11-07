379385bc9f7084e1af4a3a26f2aa211b14edefdf
sergo7777777 <sierghieigavrilov053@gmail.com>
2018-11-07 19:48:48 +0200

    HW15 SqlStorage, Resumeservlet. i think ready for 16 lesson^))))

--- src/config/Config.java
+++ src/config/Config.java
@@ -10,10 +10,10 @@ import java.io.InputStream;
 import java.util.Properties;
 
 public class Config {
-    private static final File PROPS = new File("config/resumes.properties");
+    private static final File PROPS = new File(getHomeDir(), "config/resumes.properties");
+
     private static final Config INSTANCE = new Config();
 
-    private final Properties props = new Properties();
     private final File storageDir;
     private final Storage storage;
 
@@ -23,6 +23,7 @@ public class Config {
 
     private Config() {
         try (InputStream is = new FileInputStream(PROPS)) {
+            Properties props = new Properties();
             props.load(is);
             storageDir = new File(props.getProperty("storage.dir"));
             storage = new SqlStorage(props.getProperty("db.url"), props.getProperty("db.user"), props.getProperty("db.password"));
@@ -37,5 +38,14 @@ public class Config {
 
     public Storage getStorage() {
         return storage;
+    }
+
+    private static File getHomeDir() {
+        String prop = System.getProperty("homeDir");
+        File homeDir = new File(prop == null ? "." : prop);
+        if (!homeDir.isDirectory()) {
+            throw new IllegalStateException(homeDir + " is not directory");
+        }
+        return homeDir;
     }
 }
